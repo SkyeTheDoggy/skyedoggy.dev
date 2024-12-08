1491efa4f1b967ad4135748e52213bc843567d7a
SkyeUwU <79043495+SkyeUwU@users.noreply.github.com>
2024-12-08 16:08:15 +0200

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,25 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    {
+      name: 'copy-nojekyll',
+      generateBundle() {
+        const nojekyllPath = resolve(__dirname, 'public/.nojekyll')
+
+        if (fs.existsSync(nojekyllPath)) {
+          const fileContents = fs.readFileSync(nojekyllPath, 'utf-8')
+
+          this.emitFile({
+            type: 'asset',
+            fileName: '.nojekyll',
+            source: fileContents
+          })
+        }
+      }
+    }
+  ],
   css: {
     preprocessorOptions: {
       scss: {
