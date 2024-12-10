f6b574e2ddf5d749a6ae28fbf0580b70aa1adddb
SkyeUwU <79043495+SkyeUwU@users.noreply.github.com>
2024-12-10 03:23:42 +0200

    modified:   src/app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -47,7 +47,7 @@ export function App() {
     if (routes.some(p => p.path === e.url)) {
       plausible.trackPageview()
     } else {
-      plausible.trackEvent('404 Pages', { url: e.url })
+      plausible.trackEvent('not-found-pages', { props: { url: e.url } })
     }
   };
 
